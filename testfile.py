6253b7abdbfacfd4d5f027f4252bd2834cdc8937
Taimur Khan <khan.taimur@gmail.comgit config --global user.name Taimur Khan>
2021-04-18 07:36:16 +0500

    created test program

--- testfile.py
+++ testfile.py
@@ -1,1 +1,7 @@
-#place holder
+
+
+Contractors = {"Plumbers": {"Shaan": "03152504052"}, "Electricians": {"Alauddin": "P203323670560"}}
+
+y = input("Hi, What kind of service do you require? Please type Plumbers or Electricians? ")
+
+print(Contractors[y])
